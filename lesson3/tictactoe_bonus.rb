791ff0206d49210cb8175a2238fa6a87deb0968d
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-08 07:44:37 -0500

    TicTacToe Bonus: Changed order of methods.

--- lesson3/tictactoe_bonus.rb
+++ lesson3/tictactoe_bonus.rb
@@ -1,14 +1,13 @@
 # This file is just for implementing the bonus features assignment. If you want
 # to look at what I started with, it is in /lesson3/tictactoe.rb
 
-# Bonus Feature 1: joinor implementation on line 86
+# Bonus Feature 1: joinor implementation on 302 line
 # Bonus Feature 2: Scoring
 #   - Added Menu, so we could change how many games a match we play
-#   - Added reset_score on line 307 , that adds/resets :score in game_state
-#   - Changed display_game_screen on line 154 to display score
+#   - Added initialize_score on line 269, that adds/resets :score in game_state
+#   - Changed display_game_state on line 101 to display score
 #   - Reset Score in match_loop on line 52
-#   - match_won? checks score to see if match is won on line 311
-
+#   - match_won? checks score to see if match is won on line 179
 
 require 'yaml'
 require 'io/console'
@@ -25,13 +24,14 @@ USER_MARK = 'X'
 MAGIC_SQUARE = [2, 7, 6, 9, 5, 1, 4, 3, 8]
 YES_NO = ['y', 'n']
 
+# Invoked on line 310
 def program_loop
   game_state = { match_len: 1 }
 
   loop do # program loop
     break if loop do # menu loop
       display_menu game_state
-      choice = get_specific_key MENU_KEYS
+      choice = get_filtered_keypress MENU_KEYS
 
       case choice
       when 'q' then break true
@@ -49,31 +49,31 @@ end
 def match_loop(game_state)
   loop do
     choose_first_player game_state
-    reset_score game_state
+    initialize_score game_state
 
     loop do
-      game_state[:board] = initialize_board
+      initialize_board game_state
 
       play_game(game_state)
       break if match_won? game_state
     end
-    display_match_won game_state
+    display_match_winner game_state
 
     display_strings STRINGS['again']
     display_choices(YES_NO)
-    break if get_specific_key(YES_NO) == 'n'
+    break if get_filtered_keypress(YES_NO) == 'n'
   end
 end
 
 def play_game(game_state)
   loop do
-    display_game_screen game_state
+    display_game_state game_state
 
     make_turn_mark game_state
 
     if won?(game_state)
       game_state[:score][game_state[:user_turn]] += 1
-      break display_winner(game_state)
+      break display_game_winner(game_state)
     end
 
     break display_draw(game_state) if draw?(game_state)
@@ -82,62 +82,37 @@ def play_game(game_state)
   end
 end
 
-# Bonus Feature 1: I PEDACed it in /lesson3/ttt/bonus1.rb
-def joinor(arr, sep=', ', word='or')
-  return '' if arr.size < 1
-  return arr.first.to_s if arr.size == 1
-  return "#{arr.first} #{word} #{arr.last}" if arr.size == 2
-  "#{arr[0...-1].join(sep)}#{sep}#{word} #{arr.last}"
+def display_draw(game_state)
+  display_game_state game_state
+  display_strings STRINGS['game_draw']
+
+  wait_for_keypress
 end
 
-def display_strings(strings)
-  strings = [strings] if strings.instance_of?(String)
+def display_game_winner(game_state)
+  winner = game_state[:user_turn] ? 'You' : 'Computer'
 
-  puts(strings.map { |string| string.center(TERMINAL_WIDTH) })
+  display_game_state game_state
+  display_strings format(STRINGS['game_win'], winner)
+
+  wait_for_keypress unless match_won? game_state
 end
 
-def get_specific_key(possible_keys)
-  loop do
-    key = $stdin.getch
-    break key if possible_keys.include? key
-    $stdout << "\a" # bell character
-  end
+def display_game_state(game_state)
+  $stdout.clear_screen
+
+  display_score(game_state[:score]) if game_state[:match_len] > 1
+
+  display_board(game_state[:board])
 end
 
-def wait_for_keypress
-  display_strings STRINGS['press_a_key']
-  $stdin.getch
-  nil
+def display_match_winner(game_state)
+  winner = game_state[:user_turn] ? 'You' : 'Computer'
+
+  display_strings format(STRINGS['match_win'], winner)
+
+  wait_for_keypress
 end
-
-def display_choices(choices)
-  choice_str = joinor(choices)
-  display_strings "(#{choice_str})"
-end
-
-def display_score(score)
-  display_strings format(STRINGS['score'], score[true], score[false])
-end
-
-def initialize_score
-  {true => 0, false => 0}
-end
-
-# Configuration Methods
-def change_match_len(game_state)
-  next_index = MATCH_LENGTHS.index(game_state[:match_len]) + 1
-  next_index = 0 if next_index == MATCH_LENGTHS.size
-  game_state[:match_len] = MATCH_LENGTHS[next_index]
-
-  nil
-end
-
-def get_match_len_str(length)
-  best_of = format(STRINGS['best_of'], length)
-  format(STRINGS['menu_match_len'], best_of)
-end
-
-# Screen Methods ==============================================================
 
 def display_menu(game_state)
   $stdout.clear_screen
@@ -148,49 +123,31 @@ def display_menu(game_state)
   display_strings STRINGS['menu_quit'].ljust(TITLE_WIDTH)
 end
 
-def display_game_screen(game_state)
-  $stdout.clear_screen
-
-  display_score(game_state[:score]) if game_state[:match_len] > 1
-
-  display_board(game_state[:board])
+def choose_first_player(game_state)
+  game_state[:user_turn] = flip_coin
+  display_coin_flip_animation(game_state[:user_turn])
+  display_coin_flip_winner(game_state[:user_turn])
 end
 
-def display_winner(game_state)
-  winner = game_state[:user_turn] ? 'You' : 'Computer'
+def display_board(board)
+  3.times do |row|
+    nums = (1..3).map { |num| row * 3 + num }
+    display_strings format(STRINGS['board_numbered'], *nums)
+    display_strings format(STRINGS['board_markers'],
+                           *(nums.map { |num| board[num] }))
+    display_strings STRINGS['board_empty']
+    display_strings STRINGS['board_separator'] unless row == 2
+  end
 
-  display_game_screen game_state
-  display_strings format(STRINGS['game_win'], winner)
-
-  wait_for_keypress unless match_won? game_state
+  nil
 end
 
-def display_draw(game_state)
-  display_game_screen game_state
-  display_strings STRINGS['game_draw']
-
-  wait_for_keypress
+def display_choices(choices)
+  choice_str = joinor(choices)
+  display_strings "(#{choice_str})"
 end
 
-def display_match_won(game_state)
-  winner = game_state[:user_turn] ? 'You' : 'Computer'
-
-  display_strings format(STRINGS['match_win'], winner)
-
-  wait_for_keypress
-end
-
-def display_goodbye
-  $stdout.clear_screen
-  display_strings STRINGS['goodbye']
-end
-
-# coin flip methods ============================================================
-def flip_coin
-  [true, false].sample
-end
-
-def animate_coin_flip(result)
+def display_coin_flip_animation(result)
   coin_turns = MINIMUM_COIN_TURNS
 
   loop do
@@ -210,22 +167,111 @@ def display_coin_flip_winner(result)
   wait_for_keypress
 end
 
-# board related methods ========================================================
-def initialize_board
-  Hash.new(EMPTY)
+def display_goodbye
+  $stdout.clear_screen
+  display_strings STRINGS['goodbye']
 end
 
-def display_board(board)
-  3.times do |row|
-    nums = (1..3).map { |num| row * 3 + num }
-    display_strings format(STRINGS['board_numbered'], *nums)
-    display_strings format(STRINGS['board_markers'],
-                           *(nums.map { |num| board[num] }))
-    display_strings STRINGS['board_empty']
-    display_strings STRINGS['board_separator'] unless row == 2
+def display_score(score)
+  display_strings format(STRINGS['score'], score[true], score[false])
+end
+
+def match_won?(game_state)
+  target_wins = (game_state[:match_len] / 2.0).ceil
+  game_state[:score][game_state[:user_turn]] >= target_wins
+end
+
+# I use a 3x3 Magic Square to tell if a user has won. Basically, a Magic Square
+# has all it's rows, columns, and regular diagonals sum up to the same number.
+# In the case of the 3x3 one, the sum is 15. Because of how it is set up, these
+# are the only trios of values in the square that add up to that value, so if
+# if any three squares when converted to their magic square value sum up to 15
+# we know that there was a line made!
+def won?(game_state)
+  player_mark = game_state[:user_turn] ? USER_MARK : COMPUTER_MARK
+  marks = get_player_marks(game_state[:board], player_mark)
+  marks = convert_marks_to_magic_square marks
+
+  combos = find_all_marking_combos(marks, marks, marks)
+
+  combos.select! { |combo| combo.sum == 15 }
+  combos.size > 0
+end
+
+# Because I set the board hashes default to an empty space, we don't have to
+# count empty squares as they don't technically have keys yet. So, we just
+# gotta check if all the squares are filled.
+def draw?(game_state)
+  game_state[:board].size == 9
+end
+
+def make_turn_mark(game_state)
+  if game_state[:user_turn]
+    make_user_mark game_state[:board]
+  else
+    make_computer_mark game_state[:board]
   end
+end
+
+def make_computer_mark(board)
+  empty = get_empty_marks board
+  board[empty.sample] = COMPUTER_MARK
+end
+
+def make_user_mark(board)
+  display_strings STRINGS['choose_a_square']
+  empty = get_empty_marks board
+  display_choices empty
+
+  key = get_filtered_keypress(empty.map(&:to_s)).to_i
+
+  board[key] = USER_MARK
+  nil
+end
+
+def change_match_len(game_state)
+  next_index = MATCH_LENGTHS.index(game_state[:match_len]) + 1
+  next_index = 0 if next_index == MATCH_LENGTHS.size
+  game_state[:match_len] = MATCH_LENGTHS[next_index]
 
   nil
+end
+
+def get_filtered_keypress(possible_keys)
+  loop do
+    key = $stdin.getch
+    break key if possible_keys.include? key
+    $stdout << "\a" # bell character
+  end
+end
+
+def wait_for_keypress
+  display_strings STRINGS['press_a_key']
+  $stdin.getch
+  nil
+end
+
+def get_match_len_str(length)
+  best_of = format(STRINGS['best_of'], length)
+  format(STRINGS['menu_match_len'], best_of)
+end
+
+def display_strings(strings)
+  strings = [strings] if strings.instance_of?(String)
+
+  puts(strings.map { |string| string.center(TERMINAL_WIDTH) })
+end
+
+def initialize_board(game_state)
+  game_state[:board] = Hash.new(EMPTY)
+end
+
+def initialize_score(game_state)
+  game_state[:score] = { true => 0, false => 0 }
+end
+
+def flip_coin
+  [true, false].sample
 end
 
 def get_empty_marks(board)
@@ -244,7 +290,7 @@ def convert_magic_square_to_square(magic_square)
   MAGIC_SQUARE.index(magic_square) + 1
 end
 
-def get_all_combos(arr1, arr2, arr3)
+def find_all_marking_combos(arr1, arr2, arr3)
   new_arr = []
 
   arr1.product(arr2, arr3) { |combo| new_arr << combo if combo.uniq.size == 3 }
@@ -252,71 +298,12 @@ def get_all_combos(arr1, arr2, arr3)
   new_arr
 end
 
-# Player Controller Methods ====================================================
-
-def make_turn_mark(game_state)
-  if game_state[:user_turn]
-    make_user_mark game_state[:board]
-  else
-    make_computer_mark game_state[:board]
-  end
-end
-
-def make_user_mark(board)
-  display_strings STRINGS['choose_a_square']
-  empty = get_empty_marks board
-  display_choices empty
-
-  key = get_specific_key(empty.map(&:to_s)).to_i
-
-  board[key] = USER_MARK
-  nil
-end
-
-def make_computer_mark(board)
-  empty = get_empty_marks board
-  board[empty.sample] = COMPUTER_MARK
-end
-
-# Game Flow Methods ============================================================
-
-# I use a 3x3 Magic Square to tell if a user has won. Basically, a Magic Square
-# has all it's rows, columns, and regular diagonals sum up to the same number.
-# In the case of the 3x3 one, the sum is 15. Because of how it is set up, these
-# are the only trios of values in the square that add up to that value, so if
-# if any three squares when converted to their magic square value sum up to 15
-# we know that there was a line made!
-def won?(game_state)
-  player_mark = game_state[:user_turn] ? USER_MARK : COMPUTER_MARK
-  marks = get_player_marks(game_state[:board], player_mark)
-  marks = convert_marks_to_magic_square marks
-
-  combos = get_all_combos(marks, marks, marks)
-
-  combos.select! { |combo| combo.sum == 15 }
-  combos.size > 0
-end
-
-# Because I set the board hashes default to an empty space, we don't have to
-# count empty squares as they don't technically have keys yet. So, we just
-# gotta check if all the squares are filled.
-def draw?(game_state)
-  game_state[:board].size == 9
-end
-
-def reset_score(game_state)
-  game_state[:score] = { true => 0, false => 0 }
-end
-
-def match_won?(game_state)
-  target_wins = (game_state[:match_len] / 2.0).ceil
-  game_state[:score][game_state[:user_turn]] >= target_wins
-end
-
-def choose_first_player(game_state)
-  game_state[:user_turn] = flip_coin
-  animate_coin_flip(game_state[:user_turn])
-  display_coin_flip_winner(game_state[:user_turn])
+# Bonus Feature 1: I PEDACed it in /lesson3/ttt/bonus1.rb
+def joinor(arr, sep=', ', word='or')
+  return '' if arr.size < 1
+  return arr.first.to_s if arr.size == 1
+  return "#{arr.first} #{word} #{arr.last}" if arr.size == 2
+  "#{arr[0...-1].join(sep)}#{sep}#{word} #{arr.last}"
 end
 
 # main program =================================================================
