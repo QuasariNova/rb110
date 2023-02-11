0fd336af2061b800f047be5198bfb3b64fc4d452
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-11 08:46:31 -0500

    Updated 21 with the option to play again.

--- lesson3/21.rb
+++ lesson3/21.rb
@@ -1,3 +1,5 @@
+require 'io/console'
+
 # 1. Initialize deck
 FACE_VALUES = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
 SUITS = %w(♠ ♣ ♥ ♦)
@@ -16,6 +18,7 @@ end
 #   - Display Game State
 def display_game_state(hands, hide_dealer: true)
   hidden = hide_dealer ? 1 : 0
+  $stdout.clear_screen
 
   puts("Dealer has: #{get_hand_string(hands[:dealer_hand], hidden)}")
   puts("You have: #{get_hand_string(hands[:player_hand])}")
@@ -85,6 +88,21 @@ def display_winner(winner)
   end
 end
 
+def play_again?
+  puts nil
+  puts "Do you want to play again? (yes or no)"
+  loop do
+    print "=> "
+    answer = gets.chomp.downcase
+    yes = "yes".start_with?(answer)
+    return yes if yes || "no".start_with?(answer)
+    puts "Invalid, input yes or no"
+  end
+end
+
+puts "Welcome to Twenty-One!"
+sleep(2)
+
 loop do
 # 1. Initialize deck
   deck = initialize_deck
@@ -121,7 +139,7 @@ loop do
 # 4. If player bust, dealer wins.
   if bust?(hands[:player_hand])
     puts "You busted. Dealer wins!"
-    # Ask user to play again?
+    next if play_again?
     break
   end
 
@@ -145,7 +163,7 @@ loop do
 # 6. If dealer bust, player wins.
   if bust?(hands[:dealer_hand])
     puts "Dealer busts! You win!"
-    # Ask user to play again?
+    next if play_again?
     break
   else
     puts "Dealer stayed."
@@ -156,5 +174,7 @@ loop do
 # 7. Compare cards and declare winner.
   display_winner(calculate_winner(hands))
   # Ask user to play again?
-  break
+  break unless play_again?
 end
+
+puts "Thank you for playing!"
