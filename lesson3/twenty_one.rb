fd30bcb034ec4b4dedfdf930ae9095a7bcf64f84
Christopher Perkins <Christopher.Perkins@null.net>
2023-02-12 08:59:11 -0500

    Twenty One: Bonus Feature Scoring added

--- lesson3/twenty_one.rb
+++ lesson3/twenty_one.rb
@@ -1,18 +1,33 @@
 require 'io/console'
 
-FACE_VALUES = %w(A 2 3 4 5 6 7 8 9 10 J Q K).freeze
-SUITS = %w(♠ ♣ ♥ ♦).freeze
+FACE_VALUES = %w[A 2 3 4 5 6 7 8 9 10 J Q K].freeze
+SUITS = %w[♠ ♣ ♥ ♦].freeze
 MAX_TOTAL = 21
 AI_LIMIT = 17
+TARGET_SCORE = 5
 ROUND_RESULTS = {
   draw: 'It is a draw.', player: 'You win the round!',
   dealer: 'Dealer wins the round!', player_bust: 'You busted! Dealer wins!',
   dealer_bust: 'Dealer busted! You win!'
 }.freeze
+MATCH_RESULTS = {
+  draw: 'The match ended in a draw.', player: 'You won the match!',
+  dealer: 'Dealer wins the match!'
+}.freeze
 STATE_SYMBOLS = {
   player: { hand: :player_hand, total: :player_total, score: :player_score },
   dealer: { hand: :dealer_hand, total: :dealer_total, score: :dealer_score }
 }.freeze
+
+def prompt
+  print('=> ')
+
+  gets.chomp.downcase
+end
+
+def initialize_game_state
+  { player_score: 0, dealer_score: 0 }
+end
 
 def prepare_round!(game_state)
   game_state[:deck] = FACE_VALUES.product(SUITS)
@@ -22,50 +37,6 @@ def prepare_round!(game_state)
 
   game_state[:dealer_hand] = []
   game_state[:dealer_total] = 0
-end
-
-def initialize_game_state
-  { player_score: 0, dealer_score: 0 }
-end
-
-def prompt
-  print('=> ')
-
-  gets.chomp.downcase
-end
-
-def update_total!(game_state, player, card)
-  total_symbol = STATE_SYMBOLS[player][:total]
-  hand_symbol = STATE_SYMBOLS[player][:hand]
-
-  game_state[total_symbol] += get_card_value(card)
-
-  if game_state[total_symbol] > MAX_TOTAL
-    game_state[total_symbol] = evaluate_hand(game_state[hand_symbol])
-  end
-
-  nil
-end
-
-def deal_card!(game_state, player, count = 1)
-  count.times do
-    card = game_state[:deck].delete(game_state[:deck].sample)
-    game_state[STATE_SYMBOLS[player][:hand]] << card
-    update_total!(game_state, player, card)
-  end
-
-  nil
-end
-
-def display_game_state(game_state, hide_dealer: true, hide_player: false)
-  hidden = hide_dealer ? 1 : 0
-  $stdout.clear_screen
-
-  puts("Dealer has: #{get_hand_string(game_state[:dealer_hand], hidden)}")
-  puts("You have: #{get_hand_string(game_state[:player_hand])}")
-  puts(nil)
-  puts("Your total is #{game_state[:player_total]}") unless hide_player
-  puts("The dealer's total is #{game_state[:dealer_total]}") unless hide_dealer
 end
 
 def get_hand_string(hand, hidden = 0)
@@ -80,10 +51,6 @@ def get_hand_string(hand, hidden = 0)
   cards[-1] = "and #{cards[-1]}"
 
   cards.join(separator)
-end
-
-def bust?(game_state, player)
-  game_state[STATE_SYMBOLS[player][:total]] > MAX_TOTAL
 end
 
 def get_card_value(card)
@@ -108,7 +75,34 @@ def evaluate_hand(hand)
   sum
 end
 
-def calculate_winner(game_state)
+def update_total!(game_state, player, card)
+  total_symbol = STATE_SYMBOLS[player][:total]
+  hand_symbol = STATE_SYMBOLS[player][:hand]
+
+  game_state[total_symbol] += get_card_value(card)
+
+  if game_state[total_symbol] > MAX_TOTAL
+    game_state[total_symbol] = evaluate_hand(game_state[hand_symbol])
+  end
+
+  nil
+end
+
+def update_score!(game_state, winner)
+  case winner
+  when :player, :dealer_bust then game_state[:player_score] += 1
+  when :dealer, :player_bust then game_state[:dealer_score] += 1
+  else
+    game_state[:player_score] += 0.5
+    game_state[:dealer_score] += 0.5
+  end
+end
+
+def bust?(game_state, player)
+  game_state[STATE_SYMBOLS[player][:total]] > MAX_TOTAL
+end
+
+def calculate_round_results(game_state)
   player = game_state[:player_total]
   dealer = game_state[:dealer_total]
   return :player_bust if bust?(game_state, :player)
@@ -122,23 +116,68 @@ def calculate_winner(game_state)
   :dealer
 end
 
-def display_round_winner(game_state, winner)
+def calculate_match_results(game_state)
+  return :draw if game_state[:player_score] + game_state[:dealer_score] ==
+                  2 * TARGET_SCORE
+
+  return :player if game_state[:player_score] >= TARGET_SCORE
+
+  return :dealer if game_state[:dealer_score] >= TARGET_SCORE
+
+  nil
+end
+
+def display_game_state(game_state, hide_dealer: true, hide_player: false)
+  hidden = hide_dealer ? 1 : 0
+  $stdout.clear_screen
+
+  puts("Dealer has: #{get_hand_string(game_state[:dealer_hand], hidden)}")
+  puts("You have: #{get_hand_string(game_state[:player_hand])}")
+  puts(nil)
+  puts("Your total is #{game_state[:player_total]}") unless hide_player
+  puts("The dealer's total is #{game_state[:dealer_total]}") unless hide_dealer
+end
+
+def display_match_score(game_state)
+  puts(nil)
+  puts(
+    "You: #{game_state[:player_score]}; Dealer: #{game_state[:dealer_score]} " \
+    "(Target: #{TARGET_SCORE})"
+  )
+end
+
+def display_round_results(game_state, winner)
   display_game_state(game_state, hide_dealer: false)
-  puts ROUND_RESULTS[winner]
+  puts(ROUND_RESULTS[winner])
   sleep(2)
 end
 
+def display_match_results(winner)
+  puts(nil)
+  puts(MATCH_RESULTS[winner])
+end
+
 def play_again?
-  puts nil
-  puts 'Do you want to play again? (yes or no)'
+  puts(nil)
+  puts('Do you want to play again? (yes or no)')
 
   loop do
     answer = prompt
     yes = 'yes'.start_with?(answer)
     return yes if yes || 'no'.start_with?(answer)
 
-    puts 'Invalid, please input yes or no'
+    puts('Invalid, please input yes or no')
   end
+end
+
+def deal_card!(game_state, player, count = 1)
+  count.times do
+    card = game_state[:deck].delete(game_state[:deck].sample)
+    game_state[STATE_SYMBOLS[player][:hand]] << card
+    update_total!(game_state, player, card)
+  end
+
+  nil
 end
 
 def dealer_turn!(game_state)
@@ -153,7 +192,7 @@ def dealer_turn!(game_state)
   end
   return if bust?(game_state, :dealer)
 
-  puts 'Dealer stayed.'
+  puts('Dealer stayed.')
   sleep 2
 end
 
@@ -163,7 +202,7 @@ def player_turn!(game_state)
   loop do
     display_game_state(game_state)
 
-    puts 'Please input either hit or stay.' if !is_hit
+    puts('Please input either hit or stay.') unless is_hit
 
     puts('Hit or Stay?')
     answer = prompt
@@ -186,18 +225,33 @@ def play_round!(game_state)
 
   dealer_turn!(game_state) unless bust?(game_state, :player)
 
-  display_round_winner(game_state, calculate_winner(game_state))
+  winner = calculate_round_results(game_state)
+  update_score!(game_state, winner)
+
+  display_round_results(game_state, winner)
 end
 
-puts 'Welcome to Twenty-One!'
+puts('Welcome to Twenty-One!')
 sleep(2)
 
 loop do
   game_state = initialize_game_state
 
-  play_round!(game_state)
+  loop do
+    play_round!(game_state)
+
+    winner = calculate_match_results(game_state)
+    display_match_score(game_state)
+
+    if winner
+      display_match_results(winner)
+      break
+    end
+
+    sleep(2)
+  end
 
   break unless play_again?
 end
 
-puts 'Thank you for playing!'
+puts('Thank you for playing!')
